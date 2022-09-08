12f35a822cb19ca14418872438865f8a49e791e7
Emmanuel <emmatulian@gmail.com>
2022-09-08 01:00:53 -0300

    adelantando

--- TPN2/calculadora.js
+++ TPN2/calculadora.js
@@ -1,1 +1,40 @@
-console.log('first');
+const valor = document.querySelector('.resultado');
+const valorAnterior = document.querySelector('.operacionPrevia');
+let resA = '';
+let resB = '';
+let resultado;
+let operador = '';
+const boton = document.querySelectorAll('.numero');
+boton.forEach((element) => {
+  element.addEventListener('click', () => {
+    if (operador !== '') {
+      resB += element.value;
+    } else {
+      resA += element.value;
+    }
+    valor.textContent += element.value;
+  });
+});
+const operacion = document.querySelectorAll('.operacion');
+operacion.forEach((element) =>
+  element.addEventListener('click', () => {
+    if (element.value === '+') {
+      operador = '+';
+      valor.textContent += operador;
+    }
+    if (element.value === '=') {
+      valorAnterior.textContent = resA + operador + resB;
+      valor.textContent = parseInt(resA) + parseInt(resB);
+    }
+  })
+);
+const borrar = document
+  .querySelector('.borrar')
+  .addEventListener('click', borrarResultado);
+function borrarResultado() {
+  resA = '';
+  resB = '';
+  operador = '';
+  valor.textContent = '0';
+  valorAnterior.textContent = '';
+}
